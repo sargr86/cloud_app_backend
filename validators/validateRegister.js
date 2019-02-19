339231f19d28e85132cad57cd55e55fcdd69b35f
Grigor Sargsyan <sargr86@gmail.com>
2019-02-19 12:32:27 +0400

    Added register-user validation partially, without email-exists check, integrated multer with my formData object

--- validators/validateRegister.js
+++ validators/validateRegister.js
@@ -1,5 +1,19 @@
 const rules = [
-    body('foo', 'must be not empty 2').not().isEmpty()
+    body('email').not().isEmpty().withMessage('email_required_error').isEmail().withMessage('email_invalid_error'),
+    body('password', 'password_required_error').not().isEmpty(),
+    body('gender', 'gender_required_error').not().isEmpty(),
+    body().custom(req=>{
+        let lang = req.lang;
+        let email = req.email;
+        if(req['first_name_'+lang] === '' || req['last_name_'+lang] === ''){
+            throw new Error('full_name_required_error')
+        }
+
+        else return true;
+    }),
+
+
+
 ];
 
 module.exports = {
